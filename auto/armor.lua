867ece7d7c9520d9ee8c16cb499e0417c38cd070
GN68 <81744903+GN68@users.noreply.github.com>
2025-09-11 15:20:17 +0800

    fixed chestplate being flipped

---
+++ auto/armor.lua
@@ -0,0 +1,15 @@
+models.model.base.hips.torso.ChestplatePivot:setScale(0.5,0.5,0.6)
+models.model.base.hips.torso.larm.LeftShoulderPivot:scale(0.5)
+models.model.base.hips.torso.rarm.RightShoulderPivot:scale(0.5)
+models.model.base.hips.LeggingsPivot:scale(0.7)
+models.model.base.lleg.LeftLeggingPivot:scale(0.7,0.7,0.6)
+models.model.base.lleg.lknee.LeftLeggingPivot2:scale(0.6,0.7,0.6)
+models.model.base.lleg.lknee.lfoot.LeftBootPivot:scale(0.5,0.5,0.4)
+models.model.base.lleg.lknee.lfoot.LeftBootPivot2:scale(0.53,0.5,0.4)
+
+models.model.base.hips.torso.ChestplatePivot:setRot(0,180,0)
+
+models.model.base.rleg.RightLeggingPivot:scale(0.7,0.7,0.6)
+models.model.base.rleg.rknee.RightLeggingPivot2:scale(0.6,0.7,0.6)
+models.model.base.rleg.rknee.rfoot.RightBootPivot:scale(0.5,0.5,0.4)
+models.model.base.rleg.rknee.rfoot.RightBootPivot2:scale(0.53,0.5,0.4)
