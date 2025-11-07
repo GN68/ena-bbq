1ba38cfa80bc6fdcdab3e86393b62626788fd6a4
GN68 <81744903+GN68@users.noreply.github.com>
2025-11-08 01:42:59 +0800

    added gaps to nameplate and replaced sugar with poland disc

--- auto/nameplate.lua
+++ auto/nameplate.lua
@@ -1,4 +1,4 @@
 nameplate.ENTITY
 :setBackgroundColor(0,0,0,0)
 :setOutline(true)
-nameplate.ALL:setText(":mci_sugar:${name}${badges}")
+nameplate.ALL:setText(":mci_music_disc_lava_chicken: ${name} ${badges}")
